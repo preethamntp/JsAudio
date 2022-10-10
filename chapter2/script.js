9169ed52be122ea488733b6530eaac9410326108
preethamntp <preethamntp@gmail.com>
2022-10-10 14:48:07 +0530

    chapter two with bar animation on music play

---
+++ chapter2/script.js
@@ -0,0 +1,43 @@
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas1");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+let audioSource;
+let analyser;
+
+container.addEventListener("click", () => {
+  const audio1 = document.getElementById("audio1");
+  audio1.src = "../asset/spell.wav";
+
+  const audioCtx = new AudioContext();
+
+  audio1.play();
+  audioSource = audioCtx.createMediaElementSource(audio1);
+  analyser = audioCtx.createAnalyser();
+  audioSource.connect(analyser);
+  analyser.connect(audioCtx.destination);
+  analyser.fftSize = 64;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  const barWidth = canvas.width / bufferLength;
+  let barHeight;
+  let x;
+
+  function animate() {
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+    for (let i = 0; i < bufferLength; i++) {
+      barHeight = dataArray[i];
+      ctx.fillStyle = "white";
+      ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+      x += barWidth;
+    }
+    requestAnimationFrame(animate);
+  }
+  animate();
+});
